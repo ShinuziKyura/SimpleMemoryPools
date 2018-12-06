e06868f600e1c6f96e9b3026fc530eaaaa414567
ShinuziKyura <rj.santos.pereira@gmail.com>
2018-12-06 23:08:28 +0000

    Bug fixes and implementation cleanup

--- lib/smp.hpp
+++ lib/smp.hpp
@@ -1,29 +1,29 @@
-#ifndef SIMPLE_MEMORY_POOLS_HPP
-#define SIMPLE_MEMORY_POOLS_HPP
+#ifndef SIMPLE_MEMORY_POOLS_HEADER
+#define SIMPLE_MEMORY_POOLS_HEADER
 
 #include <memory>
 #include <map>
 
-// SimpleMemoryPools - version A.1.4.0
+// SimpleMemoryPools - version A.1.4.1
 namespace smp
 {
 	namespace literals
 	{
-		constexpr size_t operator "" _B(unsigned long long size)
+		constexpr std::size_t operator "" _B(unsigned long long size)
 		{
-			return size_t(size);
+			return std::size_t(size);
 		}
-		constexpr size_t operator "" _KiB(unsigned long long size)
+		constexpr std::size_t operator "" _KiB(unsigned long long size)
 		{
-			return size_t(size << 10);
+			return std::size_t(size << 10);
 		}
-		constexpr size_t operator "" _MiB(unsigned long long size)
+		constexpr std::size_t operator "" _MiB(unsigned long long size)
 		{
-			return size_t(size << 20);
+			return std::size_t(size << 20);
 		}
-		constexpr size_t operator "" _GiB(unsigned long long size)
+		constexpr std::size_t operator "" _GiB(unsigned long long size)
 		{
-			return size_t(size << 30);
+			return std::size_t(size << 30);
 		}
 	}
 
@@ -31,8 +31,11 @@ namespace smp
 
 	class _memorypool
 	{
+		static constexpr auto ptr_equal_to = std::equal_to<std::byte *>();
+		static constexpr auto ptr_less = std::less<std::byte *>();
+		static constexpr auto ptr_less_equal = std::less_equal<std::byte *>();
 	protected:
-		_memorypool(std::byte * address, size_t size) :
+		_memorypool(std::byte * address, std::size_t size) :
 			_memorypool_address(address),
 			_memorypool_size(size),
 			_memorypool_blocks({ std::make_pair(address, size) })
@@ -44,15 +47,13 @@ namespace smp
 			_memorypool_blocks(std::move(other._memorypool_blocks))
 		{
 		}
-		_memorypool & operator=(_memorypool &&) = delete;
-		~_memorypool() = default;
 	public:
 		template <class ObjType, class ... ArgTypes>
 		ObjType * construct(ArgTypes && ... args)
 		{
 			for (auto block = std::begin(_memorypool_blocks), end_block = std::end(_memorypool_blocks); block != end_block; ++block)
 			{
-				if (auto padding = (alignof(ObjType) - (uintptr_t(block->first) % alignof(ObjType))) % alignof(ObjType);
+				if (auto const padding = (~std::ptrdiff_t(block->first) + 1) & (alignof(ObjType) - 1);
 					block->second - padding >= sizeof(ObjType))
 				{
 					auto address = block->first + padding;
@@ -90,14 +91,14 @@ namespace smp
 		template <class ObjType>
 		void destruct(ObjType * obj)
 		{
-			if (auto address = reinterpret_cast<std::byte *>(obj);
-				_memorypool_address <= address && address < _memorypool_address + _memorypool_size)
+			if (auto const address = reinterpret_cast<std::byte *>(obj);
+				ptr_less_equal(_memorypool_address, address) && ptr_less(address, _memorypool_address + std::ptrdiff_t(_memorypool_size)))
 			{
 				auto block = _memorypool_blocks.emplace(address, sizeof(ObjType)).first;
 				auto prev_block = block != std::begin(_memorypool_blocks) ? std::prev(block) : block;
 				auto next_block = std::next(block);
 
-				if (prev_block->first + prev_block->second == block->first)
+				if (ptr_equal_to(prev_block->first + std::ptrdiff_t(prev_block->second), block->first))
 				{
 					prev_block->second += block->second;
 
@@ -106,7 +107,7 @@ namespace smp
 					block = prev_block;
 				}
 
-				if (next_block != std::end(_memorypool_blocks) && block->first + block->second == next_block->first)
+				if (next_block != std::end(_memorypool_blocks) && ptr_equal_to(block->first + std::ptrdiff_t(block->second), next_block->first))
 				{
 					block->second += next_block->second;
 
@@ -115,22 +116,22 @@ namespace smp
 
 				if constexpr (std::is_class_v<ObjType>)
 				{
-					obj->~ObjType();
+					std::destroy_at(obj);
 				}
 			}
 		}
 	protected:
 		std::byte * const _memorypool_address;
-		size_t const _memorypool_size;
-		std::map<std::byte *, size_t> _memorypool_blocks;
+		std::size_t const _memorypool_size;
+		std::map<std::byte *, std::size_t> _memorypool_blocks;
 	};
 
-	template <size_t Alignment = alignof(max_align_t)>
+	template <std::size_t Alignment = alignof(std::max_align_t)>
 	class memorypool : public _memorypool
 	{
-		static_assert((Alignment & (Alignment - 1)) == 0, "Alignment must be a power of two");
+		static_assert(Alignment > 0 && (Alignment & (Alignment - 1)) == 0, "Alignment must be a power of two and greater than zero");
 	public:
-		memorypool(size_t size) :
+		memorypool(std::size_t size) :
 			_memorypool(_allocate(size), size)
 		{
 		}
@@ -144,9 +145,9 @@ namespace smp
 			_deallocate(_memorypool_address, _memorypool_size);
 		}
 	private:
-		static std::byte * _allocate(size_t size)
+		static std::byte * _allocate(std::size_t size)
 		{
-			if constexpr (Alignment > alignof(max_align_t))
+			if constexpr (Alignment > alignof(std::max_align_t))
 			{
 				return static_cast<std::byte *>(operator new(size, std::align_val_t(Alignment)));
 			}
@@ -155,9 +156,9 @@ namespace smp
 				return static_cast<std::byte *>(operator new(size));
 			}
 		}
-		static void _deallocate(std::byte * address, size_t size)
+		static void _deallocate(std::byte * address, std::size_t size)
 		{
-			if constexpr (Alignment > alignof(max_align_t))
+			if constexpr (Alignment > alignof(std::max_align_t))
 			{
 				operator delete(address, size, std::align_val_t(Alignment));
 			}
@@ -176,7 +177,7 @@ namespace smp
 		{
 		}
 	public:
-		template <size_t Alignment>
+		template <std::size_t Alignment>
 		memorypool_delete(memorypool<Alignment> * pool) :
 			memorypool_delete(static_cast<_memorypool *>(pool))
 		{
